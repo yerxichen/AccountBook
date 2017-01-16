c801b59d438f8f90781b897391662562d034f643
yer <879908188@qq.com>
2017-01-16 16:19:16 +0800

    修改主页UI

--- accountbook/src/main/java/com/example/accountbook/HomeActivity.java
+++ accountbook/src/main/java/com/example/accountbook/HomeActivity.java
@@ -1,41 +1,24 @@
 package com.example.accountbook;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity {
 
-    private TextView tv_add;
-    private TextView tv_excute;
-    private TextView tv_select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //隐藏标题
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
-        tv_add = (TextView) findViewById(R.id.tv_acc_addacc);
-        tv_excute = (TextView) findViewById(R.id.tv_acc_acclist);
-        tv_select = (TextView) findViewById(R.id.tv_acc_select);
-        tv_add.setOnClickListener(this);
-        tv_excute.setOnClickListener(this);
-        tv_select.setOnClickListener(this);
+
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.tv_acc_addacc:
-                startActivity(new Intent(this, AccountEntryActivity.class));
-                break;
-            case R.id.tv_acc_acclist:
-                startActivity(new Intent(this, AccountListActivity.class));
-                break;
-            case R.id.tv_acc_select:
-                startActivity(new Intent(this, AccountAdminActivity.class));
-        }
-    }
+
 }
